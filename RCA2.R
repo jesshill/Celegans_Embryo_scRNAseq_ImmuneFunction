2c1b33f4e3c7b41e5abb2b90e547e22f886c834b
Jessica Hill <hilllynnjessica@gmail.com>
2024-02-28 13:33:50 -0700

    Update RCA2.R

--- RCA2.R
+++ RCA2.R
@@ -1,5 +1,16 @@
-library(remotes)
+install.packages('devtools')
+library(devtools)
+
+# example
+#install_github(repo, username = NULL, ref = "master", subdir = NULL,
+#               auth_token = github_pat(quiet), host = "https://api.github.com",
+#               quiet = FALSE, ...)
+
 install_github("prabhakarlab/RCAv2", force = TRUE)
 library("RCAv2")
+
+
+
+
 
 
